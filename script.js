bdc57a0d1f9d0844cfbcb5f61dc097f10a0bd275
MZ <nikita.zholkovskyi@gmail.com>
2020-09-16 12:28:19 +0200

    found avg of one day in celsium

--- script.js
+++ script.js
@@ -10,8 +10,22 @@
        function weather(data) {
 let temp = data.list
            // console.log(temp)
-           for (let i = 0; i <  4; i++) {
-               console.log(temp[i].main.temp)
+           let tempAr =[]
+           for (let i = 0; i <  temp.length; i++) {
+               console.log(temp[i].dt_txt.split(" ")[1])
+               if (temp[i].dt_txt.split(" ")[1] === "21:00:00") {
+                 break
+               }
+               tempAr.push(temp[i].main.temp)
+               console.log(tempAr)
+               const total = tempAr.reduce(function(a, b){
+                   return a + b;
+               }, 0);
+let avgBe = total/tempAr.length
+               console.log(Math.round(avgBe))
+               // let avV = temp[i].main.temp
+               // avV += temp[i].main.temp
+               // console.log(avV)
                // let avPerDay = temp[i].main.temp / 4
                // console.log(avPerDay)
                // console.log(all)
